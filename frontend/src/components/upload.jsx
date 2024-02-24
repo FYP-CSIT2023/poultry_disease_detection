0688b54fc864d492708b864694e499e21c733c79
SONGOKU100 <gurungp599@gmail.com>
2024-02-24 13:44:50 +0545

    refactor:upload.jsx file

--- frontend/src/components/upload.jsx
+++ frontend/src/components/upload.jsx
@@ -1,49 +1,51 @@
-import { useState } from 'react';
-import axios from 'axios';
-import PropTypes from 'prop-types';
+import { useState } from "react";
+import axios from "axios";
+import PropTypes from "prop-types";
 
 const UploadButton = ({ buttonText, onUpload }) => {
   const [uploadedImage, setUploadedImage] = useState(null);
   const [imageFile, setImageFile] = useState(null);
   const [loading, setLoading] = useState(false);
-  const [responseData, setResponseData] = useState('');
+  const [responseData, setResponseData] = useState("");
 
   const handleFileChange = (e) => {
+    setResponseData("");
     const file = e.target.files[0];
     setImageFile(file);
 
     if (file) {
-      if (file.type.startsWith('image/')) {
+      if (file.type.startsWith("image/")) {
         // setUploadedImage(file);
         setUploadedImage(URL.createObjectURL(file));
         onUpload(file);
       } else {
-        alert('Please choose a valid image file.');
+        alert("Please choose a valid image file.");
       }
     }
   };
 
   const handleButtonClick = () => {
     setLoading(true);
-  
+
     // Create a FormData object to send the image file
     const formData = new FormData();
-    formData.append('image', imageFile);
+    formData.append("image", imageFile);
     console.log(imageFile);
 
     // Assuming you have a Django API endpoint at 'your-api-endpoint'
-    axios.post('http://127.0.0.1:8000/detect/uploads/', formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-    })
+    axios
+      .post("http://127.0.0.1:8000/detect/uploads/", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
       .then((response) => {
         // Handle the response data as needed
-        console.log('API Response:', response.data.predicted_class);
+        console.log("API Response:", response.data.predicted_class);
         setResponseData(response.data.predicted_class);
       })
       .catch((error) => {
-        console.error('Error:', error);
+        console.error("Error:", error);
       })
       .finally(() => {
         setLoading(false);
@@ -51,69 +53,69 @@ const UploadButton = ({ buttonText, onUpload }) => {
   };
 
   const styles = {
-    UploadButtonContainer : {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      flexDirection:'column',
-      height: '85vh',
-      border: '1px solid #3498db',
-      },
-  
+    UploadButtonContainer: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      flexDirection: "column",
+      height: "85vh",
+      border: "1px solid #3498db",
+    },
+
     label: {
-      cursor: 'pointer',
-      backgroundColor: '#3498db',
-      color: '#fff',
-      padding: '10px 20px',
-      borderRadius: '5px',
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      fontSize: '24px',
+      cursor: "pointer",
+      backgroundColor: "#3498db",
+      color: "#fff",
+      padding: "10px 20px",
+      borderRadius: "5px",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      fontSize: "24px",
     },
- 
+
     labelImg: {
-      maxWidth: '50px',
-      marginBottom: '10px',
+      maxWidth: "50px",
+      marginBottom: "10px",
     },
-  
+
     uploadedImageContainer: {
-      display :'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      marginTop: '20px',
-      textAlign: 'center',
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      marginTop: "20px",
+      textAlign: "center",
     },
-    
+
     uploadedImage: {
-      maxWidth: '150px',
-      borderRadius: '5px',
-      boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+      maxWidth: "150px",
+      borderRadius: "5px",
+      boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
     },
-  
-    uploadedImageLabel:{
-      marginBottom: '20px',
-      fontSize: '24px',
+
+    uploadedImageLabel: {
+      marginBottom: "20px",
+      fontSize: "24px",
       fontWeight: 600,
     },
-    uploadImageContainer:{
-      display: 'flex',
-      flexDirection: 'column',
-      gap: '15px',
-      justifyContent: 'center',
-      alignItems: 'center',
+    uploadImageContainer: {
+      display: "flex",
+      flexDirection: "column",
+      gap: "15px",
+      justifyContent: "center",
+      alignItems: "center",
     },
     predictClassTxt: {
-      fontSize: '30px',
-      color: '#3498db',
+      fontSize: "30px",
+      color: "#3498db",
     },
-    detectBtn:{
-      fontSize: '24px',
-      padding: '10px 20px',
-      backgroundColor: '#49be25',
-      borderRadius: '5px',
-      color: '#fff',
-    }
+    detectBtn: {
+      fontSize: "24px",
+      padding: "10px 20px",
+      backgroundColor: "#49be25",
+      borderRadius: "5px",
+      color: "#fff",
+    },
   };
 
   return (
@@ -121,7 +123,11 @@ const UploadButton = ({ buttonText, onUpload }) => {
       <div>
         <label>
           <p style={styles.uploadedImageLabel}>{buttonText} </p>
-          <input type="file" style={{ display: 'none' }} onChange={handleFileChange} />
+          <input
+            type="file"
+            style={{ display: "none" }}
+            onChange={handleFileChange}
+          />
           <div style={styles.label}>Choose a File</div>
         </label>
       </div>
@@ -130,11 +136,23 @@ const UploadButton = ({ buttonText, onUpload }) => {
         {uploadedImage && (
           <div style={styles.uploadImageContainer}>
             <p style={styles.uploadedImageLabel}>Uploaded Image :</p>
-            <img src={uploadedImage} alt="Uploaded" style={styles.uploadedImage} />
-            <button style={styles.detectBtn} onClick={handleButtonClick} disabled={loading}>
-              {loading ? 'Loading...' : 'Detect'}
+            <img
+              src={uploadedImage}
+              alt="Uploaded"
+              style={styles.uploadedImage}
+            />
+            <button
+              style={styles.detectBtn}
+              onClick={handleButtonClick}
+              disabled={loading}
+            >
+              {loading ? "Loading..." : "Detect"}
             </button>
-            {responseData && <p style={styles.predictClassTxt}>The predicted class is: {responseData}</p>}
+            {responseData && (
+              <p style={styles.predictClassTxt}>
+                The predicted class is: {responseData}
+              </p>
+            )}
           </div>
         )}
       </div>
