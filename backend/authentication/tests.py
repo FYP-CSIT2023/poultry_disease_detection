90685d98c6546ec51e5938dbcc23a052326ab053
SONGOKU100 <gurungp599@gmail.com>
2024-03-02 02:58:21 +0545

    feat: add unit test and refactor code

--- backend/authentication/tests.py
+++ backend/authentication/tests.py
@@ -1,3 +1,54 @@
+from rest_framework.test import APIClient
+from rest_framework import status
+#from rest_framework.test import APITestCase
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your tests here.
+
+class AuthenticationTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.register_url = reverse('register')
+        self.login_url = reverse('login')
+        self.logout_url = reverse('logout')
+
+    def test_registration(self):
+        data = {
+            'username': 'testuser',
+            'email': 'test@example.com',
+            'password': 'testpassword'
+        }
+
+        response = self.client.post(self.register_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        
+
+    def test_login(self):
+        user = get_user_model().objects.create_user(
+            username='testuser',
+            email='test@example.com',
+            password='testpassword'
+        )
+
+        data = {
+            'username': 'testuser',
+            'password': 'testpassword'
+        }
+
+        response = self.client.post('/api/login/', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('access', response.data)
+        self.assertIn('refresh', response.data)
+
+    def test_login_with_wrong_credentials(self):
+        data = {
+            'username': 'nonexistentuser',
+            'password': 'wrongpassword'
+        }
+
+        response = self.client.post('/api/login/', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+   
